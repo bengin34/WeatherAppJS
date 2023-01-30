56698505dd17b6d4eb7b5f539097c073683f137a
bengin34 <becaglar3434@gmail.com>
2023-01-30 11:37:21 +0300

    done

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const msg = document.querySelector(".form-msg");
 const list = document.querySelector(".cities");
 
 //API KEY
-const apiKey = "43f3d3471358a0770f15f24ec227b978";
+const apiKey =  "43f3d3471358a0770f15f24ec227b978";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -59,9 +59,49 @@ form.addEventListener("submit", (e) => {
   //AJAX magic
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}&units=metric`;
 
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-    });
-});
+
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			// If we get a 404 code, throw an error
+			if (data.cod == '404') {
+				throw new Error(`${data.cod}, ${data.message}`)
+			}
+
+			// Let's destructure the data object
+			const {main, name, sys, weather} = data
+
+			// Define the icon location
+			const icon = `./icons/${weather[0].icon}.png`
+
+			// Create the list item for the new city
+			const li = document.createElement('li')
+
+			// Define markup
+			const markup = `
+				<figure>
+					<img src="${icon}" alt="${weather[0]['description']}">
+				</figure>
+				<div>
+					<h2>${Math.round(main.temp)}<sup>°C</sup></h2>
+					<p class="city__conditions">${weather[0]['description'].toUpperCase()}</p>
+					<h3><span class="city__name">${name}</span><span class="city__country">${sys.country}</span></h3>
+				</div>
+			`
+
+			// Add the new markup to the list item
+			li.innerHTML = markup
+
+			// Add the new list item to the page
+			list.appendChild(li)
+		})
+		.catch(() => {
+			msg.textContent = 'Please search for a valid city!'
+			msg.classList.add('visible')
+		})
+
+	msg.textContent = ''
+
+	form.reset()
+	input.focus()
+})
